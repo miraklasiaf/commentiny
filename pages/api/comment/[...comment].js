98234d75ab54944e5ac320ca46cdd6729241c846
Faisal Karim <faisalkarim96@gmail.com>
2020-09-19 12:35:15 +0700

    chore: add husky and lintstaged

--- pages/api/comment/[...comment].js
+++ pages/api/comment/[...comment].js
@@ -1,11 +1,12 @@
-import { getAllComment } from '@/lib/db-admin';
+import { getAllComment } from '@/lib/db-admin'
 
 export default async (req, res) => {
   try {
-    const [siteId, route] = req.query.site;
-    const { comment } = await getAllComment(siteId, route);
+    const [siteId, route] = req.query.site
+    const { comment } = await getAllComment(siteId, route)
 
-    res.status(200).json({ comment });
+    res.status(200).json({ comment })
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(500).json({ error })
   }
+}
